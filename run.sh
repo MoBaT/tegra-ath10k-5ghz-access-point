076964f833faed97b9feefb936bc80ebe75b98e9
Moe Bataineh <mobataineh@yahoo.com>
2022-04-15 18:28:45 -0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -15,7 +15,7 @@ if ! : >/dev/tcp/8.8.8.8/53; then
 	exit
 fi
 
-sudo apt-get install crda hostapd dnsmasq
+sudo apt-get install -y crda hostapd dnsmasq
 
 SCRIPT_LOCATION="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 JETSON_L4T_STRING=$(head -n 1 /etc/nv_tegra_release)
